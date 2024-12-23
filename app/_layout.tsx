1b4fe1d4d6ec0493896608ade0ca2de04c372021
DolenDeori <dulench.deori565@gmail.com>
2024-12-23 14:21:22 +0530

    Changed Initial Code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,26 +5,12 @@ import { useEffect } from "react";
 import "react-native-reanimated";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+// SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
   );
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Onboarding = () => {
+  return (
+    <SafeAreaView>
+      <View>Hello</View>
+    </SafeAreaView>
+  );
+};
+
+export default Onboarding;
