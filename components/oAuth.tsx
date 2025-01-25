463bce92d2ad161ade705f8636aff02809ea6df3
DolenDeori <dulench.deori564@gmail.com>
2025-01-26 00:50:17 +0530

    IMPROVEMENT: Made the OAuth functions as a new compoent.

---
+++ components/oAuth.tsx
@@ -0,0 +1,35 @@
+import { Alert, Image, Text, View } from "react-native";
+import CustomButton from "./customButton";
+import { icons } from "@/constants";
+
+const OAuth = () => {
+  const handleGoogleSignIn = () => {
+    Alert.alert("Google Sign-In", "This feature is under development.");
+  };
+  return (
+    <View>
+      {/* OR Separator */}
+      <View className="flex-row items-center my-6">
+        <View className="flex-1 h-px bg-gray-300" />
+        <Text className="mx-4 text-gray-500 font-DMSansRegular">OR</Text>
+        <View className="flex-1 h-px bg-gray-300" />
+      </View>
+
+      {/* Google OAuth Button */}
+      <CustomButton
+        title="Sign in with Google"
+        onPress={handleGoogleSignIn}
+        bgVariant="outline"
+        textVariant=""
+        IconLeft={() => (
+          <Image
+            source={icons.google}
+            resizeMode="contain"
+            className="w-5 h-5 mx-2"
+          />
+        )}
+      />
+    </View>
+  );
+};
+export default OAuth;
