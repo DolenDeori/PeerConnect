51a23059047a7286f0a9e539fb4797cf59689005
DolenDeori <dulench.deori564@gmail.com>
2025-01-26 00:33:24 +0530

    UPDATE: Include OAuth Option in Sign In Page

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,8 +1,16 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, Alert, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Alert,
+  ScrollView,
+  Image,
+} from "react-native";
 import InputField from "@/components/inputField";
 import CustomButton from "@/components/customButton";
 import { router } from "expo-router";
+import { icons } from "@/constants";
 
 const SignIn = () => {
   const [userName, setUserName] = useState("");
@@ -13,6 +21,9 @@ const SignIn = () => {
     setError("");
     Alert.alert("Signup Successful", "Welcome to the app!");
     router.push("../(root)/(tabs)/home");
+  };
+  const handleGoogleSignIn = () => {
+    Alert.alert("Google Sign-In", "This feature is under development.");
   };
 
   return (
@@ -48,8 +59,30 @@ const SignIn = () => {
         {/* Submit Button */}
         <CustomButton title="Sign In" className="mt-8" onPress={handleSignup} />
 
+        {/* OR Separator */}
+        <View className="flex-row items-center my-6">
+          <View className="flex-1 h-px bg-gray-300" />
+          <Text className="mx-4 text-gray-500 font-DMSansRegular">OR</Text>
+          <View className="flex-1 h-px bg-gray-300" />
+        </View>
+
+        {/* Google OAuth Button */}
+        <CustomButton
+          title="Sign in with Google"
+          onPress={handleGoogleSignIn}
+          bgVariant="outline"
+          textVariant=""
+          IconLeft={() => (
+            <Image
+              source={icons.google}
+              resizeMode="contain"
+              className="w-5 h-5 mx-2"
+            />
+          )}
+        />
+
         {/* Login Link */}
-        <View className="mt-4 flex-row justify-center">
+        <View className="mt-4 mb-8 flex-row justify-center">
           <Text className="text-sm text-gray-500 font-DMSansRegular">
             Don't have an account?{" "}
           </Text>
@@ -59,7 +92,7 @@ const SignIn = () => {
             }}
           >
             <Text className="text-sm text-purple-600 font-DMSansSemiBold">
-              Sign up
+              Sign Up
             </Text>
           </TouchableOpacity>
         </View>
