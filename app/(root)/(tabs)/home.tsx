bcb7680a123d8f264da8ea3c88bf839176ccb8e1
DolenDeori <dulench.deori564@gmail.com>
2025-01-27 21:10:34 +0530

    feat: Modify package.json scripts for Android and iOS commands; enhance Home component layout and text

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,6 +1,6 @@
 import CustomButton from "@/components/customButton";
 import { useState } from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
@@ -11,13 +11,22 @@ const Home = () => {
   };
   return (
     <SafeAreaView className="flex flex-1 h-full w-full bg-gray-200 items-center justify-center">
-      <Text className="font-HostGorteskBold text-3xl">This is Home Page</Text>
-      <CustomButton
-        title={`Click to Count ${count}`}
-        className="mt-5"
-        onPress={hanleClick}
-        bgVariant="success"
-      />
+      <Text className="font-HostGorteskBold text-3xl">
+        Welcome To Peer Connect
+      </Text>
+      <View className="flex flex-row gap-4 mx-8">
+        <CustomButton
+          title={`I am Travlling`}
+          className="mt-5 flex-1"
+          onPress={hanleClick}
+        />
+        <CustomButton
+          title={`I am Sending`}
+          className="mt-5 flex-1"
+          onPress={hanleClick}
+          bgVariant="success"
+        />
+      </View>
     </SafeAreaView>
   );
 };
