8d6027f2d376723d2f88ebd61c9cda0b8ee83de6
DolenDeori <dulench.deori564@gmail.com>
2025-01-29 00:41:52 +0530

    feat: Add contact and profile tabs with layout adjustments; update package.json for new dependencies

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,10 +1,65 @@
-import { Stack } from "expo-router";
+import { Stack, Tabs } from "expo-router";
+import { View } from "react-native";
+import { HomeIcon, ChartPieIcon } from "react-native-heroicons/outline";
+
+const TabIcon = ({ focused }: { focused: boolean }) => (
+  <View
+    className={` rounded-full w-12 h-12 flex justify-center items-center mb-5 ${
+      focused ? "bg-general-400" : ""
+    }`}
+  >
+    <HomeIcon color={"black"} />
+  </View>
+);
 
 const Layout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="home" options={{ headerShown: false }} />
-    </Stack>
+    <Tabs
+      initialRouteName="home"
+      screenOptions={{
+        tabBarActiveTintColor: "white",
+        tabBarInactiveTintColor: "white",
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "#1E1E1E",
+          position: "absolute",
+          display: "flex",
+          overflow: "hidden",
+          height: 66,
+          paddingTop: 23,
+          marginBottom: 20,
+          marginHorizontal: 20,
+          paddingBottom: 0,
+          paddingHorizontal: 0,
+          borderRadius: 90,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => <HomeIcon color={"black"} />,
+        }}
+      />
+      <Tabs.Screen
+        name="contact"
+        options={{
+          title: "Contact",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => <ChartPieIcon color={"black"} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => <TabIcon focused={focused} />,
+        }}
+      />
+    </Tabs>
   );
 };
 export default Layout;
