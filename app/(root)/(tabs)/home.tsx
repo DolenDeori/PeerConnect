fedf21f80c1a6d673baf84d3360a46f8aa2c7e59
DolenDeori <dulench.deori564@gmail.com>
2025-03-06 01:53:44 +0530

    feat: personalize welcome message on home screen with user information

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -3,31 +3,26 @@ import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { router } from "expo-router";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { useUser } from "@clerk/clerk-expo";
+import { useState } from "react";
 
 const mapAPI = process.env.GOOGLE_PLACES_API_KEY;
 
 const Home = () => {
+  const { user } = useUser();
+  const loading = false;
+  const [hasPermissions, setHasPermissions] = useState(false);
+
   const handleSendingClick = () => {
-    router.push("/package-details"); // Navigate to the PackageDetailsScreen
+    router.push("/package-details");
   };
 
   return (
     <SafeAreaView className="flex flex-1 h-full w-full bg-gray-200 items-center justify-center">
-      {/* <View>
-        <GooglePlacesAutocomplete
-          placeholder="Search"
-          query={{
-            key: `${mapAPI}`,
-            language: "en",
-          }}
-          textInputProps={{
-            placeholderTextColor: "gray",
-            placeholder: "Where to go",
-          }}
-        />
-      </View> */}
       <Text className="font-HostGorteskBold text-3xl">
-        Welcome To Peer Connect
+        Welcome{" "}
+        {user?.firstName || user?.emailAddresses[0].emailAddress.split("@")[0]}{" "}
+        To Peer Connect
       </Text>
       <View className="flex flex-row gap-4 mx-8">
         <CustomButton
