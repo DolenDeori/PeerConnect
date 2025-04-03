e6a700736eeef45fa8c11cc5e17b1d0b57011eb6
DolenDeori <dulench.deori564@gmail.com>
2025-04-03 23:35:45 +0530

    feat: Implement user profile data fetching and display in account settings

--- app/(root)/account-settings.tsx
+++ app/(root)/account-settings.tsx
@@ -1,11 +1,86 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TextInput, Alert, ActivityIndicator } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useUser } from "@clerk/clerk-expo";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "@/firebaseConfig";
 
 const AccountSettings = () => {
+  const { user } = useUser();
+  const [userData, setUserData] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const swiperRef = useRef(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      if (!user || !user.id) {
+        Alert.alert("Error", "User is not authenticated");
+        setLoading(false);
+        return;
+      }
+      try {
+        // Get the document reference using the Clerk user ID
+        const userDocRef = doc(db, "users", user.id);
+        const docSnap = await getDoc(userDocRef);
+        if (docSnap.exists()) {
+          setUserData(docSnap.data());
+        } else {
+          Alert.alert(
+            "No Data",
+            "User profile does not exist in the database."
+          );
+        }
+      } catch (error: any) {
+        Alert.alert("Error Fetching Profile", error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  if (loading) {
+    return (
+      <SafeAreaView className="flex-1 justify-center items-center bg-white">
+        <ActivityIndicator size="large" color="#0000ff" />
+      </SafeAreaView>
+    );
+  }
   return (
-    <View>
-      <Text>AccountSettings</Text>
-    </View>
+    <SafeAreaView className="flex-1 p-4 bg-white">
+      <View>
+        <Text className="font-HostGorteskMedium text-xl">
+          Your account information
+        </Text>
+      </View>
+      <View className="mt-6 gap-4">
+        <View>
+          <Text className="font-DMSansSemiBold">Username</Text>
+          <TextInput
+            placeholder={userData.userName}
+            className="bg-gray-50 font-DMSansRegular px-2 py-4 mt-2 rounded-xl"
+            editable={false}
+          />
+        </View>
+        <View>
+          <Text className="font-DMSansSemiBold">Email</Text>
+          <TextInput
+            placeholder={userData.email}
+            className="bg-gray-50 font-DMSansRegular px-2 py-4 mt-2 rounded-xl"
+            editable={false}
+          />
+        </View>
+        <View>
+          <Text className="font-DMSansSemiBold">Phone Number</Text>
+          <TextInput
+            placeholder={`+91-${userData.phoneNumber}`}
+            className="bg-gray-50 font-DMSansRegular px-2 py-4 mt-2 rounded-xl"
+            editable={false}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
