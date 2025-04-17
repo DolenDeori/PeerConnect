57442dd5ebe75693a8fb43900530ee989364988d
DolenDeori <dulench.deori564@gmail.com>
2025-04-17 23:56:19 +0530

    feat: Update Step1 and PackageHistory components; enhance BottomSheet snap points and integrate GestureHandlerRootView

--- app/(root)/(sender)/multistep/step1.tsx
+++ app/(root)/(sender)/multistep/step1.tsx
@@ -1,11 +1,10 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React, { useMemo } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { LocateFixed } from "lucide-react-native";
 import BottomSheet from "@gorhom/bottom-sheet";
 
 const Step1 = () => {
-  const snapPoints = useMemo(() => ["25%", "50%"], []);
+  const snapPoints = useMemo(() => ["25%", "50%", "70%"], []);
   return (
     <SafeAreaView className="flex-1 bg-white p-4">
       <View className="mt-4">
@@ -13,17 +12,6 @@ const Step1 = () => {
         <Text className="font-DMSansRegular">
           Provide your location information
         </Text>
-      </View>
-      <View className="mt-6">
-        <TouchableOpacity className="p-4 flex flex-row items-center justify-center gap-2 bg-gray-200 rounded-xl mb-2">
-          <LocateFixed color={"black"} size={20} />
-          <Text>Add Pickup Location</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity className="p-4 flex flex-row items-center justify-center gap-2 bg-gray-200 rounded-xl mb-2">
-          <LocateFixed color={"black"} size={20} />
-          <Text>Add Pickup Location</Text>
-        </TouchableOpacity>
       </View>
       <BottomSheet snapPoints={snapPoints}>
         <Text>Hello I am a Bottom Sheet</Text>
--- app/(root)/(tabs)/package.tsx
+++ app/(root)/(tabs)/package.tsx
@@ -1,11 +1,26 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useMemo } from "react";
+import BottomSheet from "@gorhom/bottom-sheet";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 const PackageHistory = () => {
+  const snapPoints = useMemo(() => ["25%", "50%", "70%"], []);
+  const bottomSheetRef = React.useRef<BottomSheet>(null);
+
+  React.useEffect(() => {
+    bottomSheetRef.current?.expand();
+  }, []);
+
   return (
-    <View>
-      <Text>PackageHistory</Text>
-    </View>
+    <GestureHandlerRootView>
+      <View>
+        <BottomSheet ref={bottomSheetRef} snapPoints={snapPoints}>
+          <View>
+            <Text>Hello I am a Bottom Sheet</Text>
+          </View>
+        </BottomSheet>
+      </View>
+    </GestureHandlerRootView>
   );
 };
 
