71c12805d722ba431ac4e6c9e6f5202336f55f89
DolenDeori <dulench.deori564@gmail.com>
2025-04-17 22:57:56 +0530

    feat: Enhance multistep layout with navigation and progress bar; update Step1 component with bottom sheet and location inputs

--- app/(root)/(sender)/multistep/_layout.tsx
+++ app/(root)/(sender)/multistep/_layout.tsx
@@ -1,13 +1,56 @@
-import { Stack } from "expo-router";
+import { router, Stack, useSegments } from "expo-router";
+import { Text, Touchable, TouchableOpacity, View } from "react-native";
+import StepProgressBar from "@/components/stepProgressBar";
+import { ChevronLeft } from "lucide-react-native";
+import CustomButton from "@/components/customButton";
+import React from "react";
+
+const steps = ["Location", "Package", "Receiver", "Summary"];
 
 const Layout = () => {
+  const segments = useSegments(); // get the current route segments
+
+  const currentSegment = segments[segments.length - 1]; // e.g., "step2"
+  const currentStepIndex = steps.findIndex(
+    (_, i) => `step${i + 1}` === currentSegment
+  );
+
+  const route = `/multistep/step${currentStepIndex + 2}`;
+
   return (
-    <Stack>
-      <Stack.Screen name="step1" options={{ headerShown: false }} />
-      <Stack.Screen name="step2" options={{ headerShown: false }} />
-      <Stack.Screen name="step3" options={{ headerShown: false }} />
-      <Stack.Screen name="step4" options={{ headerShown: false }} />
-    </Stack>
+    <View className="bg-white flex-1">
+      <TouchableOpacity
+        onPress={() => router.back()}
+        className="flex-row gap-1 items-center p-2"
+      >
+        <ChevronLeft color={"black"} size={20} />
+        <Text className="font-HostGorteskMedium">Back</Text>
+      </TouchableOpacity>
+
+      <StepProgressBar currentStep={currentStepIndex} steps={steps} />
+
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          animation: "slide_from_right",
+          gestureEnabled: true,
+        }}
+      >
+        <Stack.Screen name="step1" options={{ headerShown: false }} />
+        <Stack.Screen name="step2" options={{ headerShown: false }} />
+        <Stack.Screen name="step3" options={{ headerShown: false }} />
+        <Stack.Screen name="step4" options={{ headerShown: false }} />
+      </Stack>
+
+      <View className="p-4">
+        {currentStepIndex <= 3 && (
+          <CustomButton
+            title={currentStepIndex === 3 ? "Submit" : "Next"}
+            onPress={() => router.push(route)}
+          />
+        )}
+      </View>
+    </View>
   );
 };
 
--- app/(root)/(sender)/multistep/step1.tsx
+++ app/(root)/(sender)/multistep/step1.tsx
@@ -1,47 +1,33 @@
-import { View, Text, ScrollView, TouchableOpacity } from "react-native";
-import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useMemo } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ChevronLeft } from "lucide-react-native";
-import { router } from "expo-router";
-import CustomButton from "@/components/customButton";
-import { useForm } from "@/app/contex/FormContex";
-import GoogleTextInput from "@/components/googleTextInput";
+import { LocateFixed } from "lucide-react-native";
+import BottomSheet from "@gorhom/bottom-sheet";
 
 const Step1 = () => {
-  const { formData, updateFormData } = useForm();
-  const [pickupPoint, setPickupPoint] = useState(
-    formData.locationInfo.pickupPoint
-  );
-  const [deliveryPoint, setDeliveryPoint] = useState(
-    formData.locationInfo.deliveryPoint
-  );
-
-  const handleNext = () => {
-    updateFormData("locationInfo", { pickupPoint, deliveryPoint });
-    router.push("/multistep/step2");
-  };
-
+  const snapPoints = useMemo(() => ["25%", "50%"], []);
   return (
     <SafeAreaView className="flex-1 bg-white p-4">
-      <TouchableOpacity onPress={() => router.back()}>
-        <View className="flex-row items-center py-2">
-          <ChevronLeft color={"black"} />
-          <Text>Back</Text>
-        </View>
-      </TouchableOpacity>
       <View className="mt-4">
-        <Text className="text-3xl font-HostGorteskBold">Send Package</Text>
+        <Text className="text-3xl font-HostGorteskBold">Send Your Package</Text>
         <Text className="font-DMSansRegular">
-          Please provide your location information
+          Provide your location information
         </Text>
       </View>
       <View className="mt-6">
-        <GoogleTextInput placeholder="Select Pickup Location" />
-        <GoogleTextInput placeholder="Select Delivery Location" />
+        <TouchableOpacity className="p-4 flex flex-row items-center justify-center gap-2 bg-gray-200 rounded-xl mb-2">
+          <LocateFixed color={"black"} size={20} />
+          <Text>Add Pickup Location</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity className="p-4 flex flex-row items-center justify-center gap-2 bg-gray-200 rounded-xl mb-2">
+          <LocateFixed color={"black"} size={20} />
+          <Text>Add Pickup Location</Text>
+        </TouchableOpacity>
       </View>
-      <View className="py-2">
-        <CustomButton title="Next" onPress={handleNext} />
-      </View>
+      <BottomSheet snapPoints={snapPoints}>
+        <Text>Hello I am a Bottom Sheet</Text>
+      </BottomSheet>
     </SafeAreaView>
   );
 };
