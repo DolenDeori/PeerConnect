549913d9cedeadf84d10376921fe6093c7182bfe
DolenDeori <dulench.deori564@gmail.com>
2025-04-24 12:59:44 +0530

    feat: Implement navigation handling and pickup location update in Step1 component

--- app/(root)/(sender)/multistep/step1.tsx
+++ app/(root)/(sender)/multistep/step1.tsx
@@ -1,17 +1,32 @@
 import { View, Text } from "react-native";
-import React, { useMemo } from "react";
-import CustomBottomSheet from "@/components/customBottomSheet";
-import { TextInput } from "react-native-gesture-handler";
+import React, { useCallback, useEffect } from "react";
 import GoogleTextInput from "@/components/googleTextInput";
 import { useFormStore } from "@/store";
 import { LocationFromGoogle } from "@/types/type";
+import { router } from "expo-router";
+import { useNavigationStore } from "@/store";
 
 const Step1 = () => {
-  const snapPoints = useMemo(() => ["25%", "50%", "70%"], []);
+  const { setHandleNext } = useNavigationStore();
   const updatePickupLocation = useFormStore(
     (state) => state.updatePickupLocation
   );
   const pickupLocation = useFormStore((state) => state.data.pickupLocation);
+
+  const handleNext = useCallback(() => {
+    console.log("Step 1 → updating form data");
+    updatePickupLocation(pickupLocation as LocationFromGoogle);
+    router.push("/step2");
+  }, []);
+
+  useEffect(() => {
+    setHandleNext(() => handleNext); // set the handleNext function in the store
+
+    return () => {
+      setHandleNext(null); // clean up the handleNext function when the component unmounts
+    };
+  }, [handleNext]);
+
   return (
     <View className="flex-1 p-4 bg-white">
       <Text>Thi is sender page</Text>
